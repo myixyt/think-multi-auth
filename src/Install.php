b5aa4b68c6cb9675eb80ad2febbfae352b3f7a59
Yu <myixyt@gmail.com>
2024-04-24 15:53:04 +0800

    install

--- src/Install.php
+++ src/Install.php
@@ -38,13 +38,12 @@ class Install
     {
         foreach (static::$pathRelation as $source => $dest) {
             if ($pos = strrpos($dest, '/')) {
-                $parent_dir = base_path() . '/' . substr($dest, 0, $pos);
+                $parent_dir = root_path() . '/' . substr($dest, 0, $pos);
                 if (!is_dir($parent_dir)) {
                     mkdir($parent_dir, 0777, true);
                 }
             }
-            //symlink(__DIR__ . "/$source", base_path()."/$dest");
-            copy_dir(__DIR__ . "/$source", base_path() . "/$dest");
+            copy_dir(__DIR__ . "/$source", root_path() . "/$dest");
         }
     }
 
@@ -55,13 +54,10 @@ class Install
     public static function uninstallByRelation()
     {
         foreach (static::$pathRelation as $source => $dest) {
-            $path = base_path() . "/$dest";
+            $path = root_path() . "/$dest";
             if (!is_dir($path) && !is_file($path)) {
                 continue;
             }
-            /*if (is_link($path) {
-                unlink($path);
-            }*/
             remove_dir($path);
         }
     }
